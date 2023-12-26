d967f4678233837b05b96f3ea8023d487c8cf8cf
marjoriepolistico <marjpolistico21@gmail.com>
2023-12-26 10:19:15 +0800

    Author and Editor Middleware

--- routes/api.php
+++ routes/api.php
@@ -25,104 +25,95 @@ use Illuminate\Support\Facades\Route;
 
 /* Public API */
 
-Route::post('/register', [UserProfilesController::class, 'store'])->name('profile.store');;
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/login',                     'login')->name('profile.login');
-});
-Route::post('/logout', [AuthController::class, 'logout']);
-
-// Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
-    
-//     //Route::get('/logout', [AuthController::class, 'logout']);
-
-//     Route::controller(UserProfilesController::class)->group(function () {
-//         Route::get('/profile',                          'index');
-//         Route::get('/profile/{id}',                     'show');
-//         Route::put('/profile/{id}',                     'password')->name('profile.password');
-//         Route::delete('/profile/{id}',                  'destroy');
-//     });
-    
-
-//     return $request->user();
-    
-
-// });
-
-
+Route::post('/register', [UserProfilesController::class,    'store'])->name('profile.store');
+Route::post('/login',    [AuthController::class,            'login'])->name('profile.login');
 
 
 /* Private API */
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    /* User Management */ //Manage by Editorial Staff
-    Route::controller(UserProfilesController::class)->group(function () {
-        Route::get('/user',                              'index');         //Shows ALL Users
-        Route::get('/user/{id}',                         'show');          //shows based on the id
-        Route::delete('/user/{id}',                      'destroy');       //delete user
-    });
-
-    /* User Specific Info */ //Common To all
+    /* COMMON ROUTES */
+    /* User Specific Info */
     Route::controller(UserProfilesController::class)->group(function () {
         Route::get('/profile',                           'showUser');
         Route::put('/profile/password',                  'password')->name('profile.password');     //change password
         Route::put('/profile/update-info',               'updateInfo') -> name ('profile.updateInfo');      //update user info
     });
 
-     /* Author Management */ //Manage by Editorial Staff
-    Route::controller(AuthorsController::class)->group(function () {
-        Route::get('/author',                          'index');
-        Route::get('/author/{id}',                     'show');
-        Route::delete('/author/{id}',                  'destroy');
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+     /* Editor only routes */
+    Route::middleware('editor')->group(function () {
+
+        /* User Management */
+        Route::controller(UserProfilesController::class)->group(function () {
+            Route::get('/user',                              'index');         //Shows ALL Users
+            Route::get('/user/{id}',                         'show');          //shows based on the id
+            Route::delete('/user/{id}',                      'destroy');       //delete user
+        });
+
+        /* Author Management */
+        Route::controller(AuthorsController::class)->group(function () {
+            Route::get('/author',                          'index');
+            Route::get('/author/{id}',                     'show');
+            Route::delete('/author/{id}',                  'destroy');
+        });
+
+        /* Editor Management */
+        Route::controller(EditorialStaffsController::class)->group(function () {
+            Route::get('/editor',                          'index');
+            Route::get('/editor/{id}',                     'show');
+            Route::delete('/editor/{id}',                  'destroy');
+        });
+
+        
+        Route::controller(PublicationsController::class)->group(function () {
+            Route::get('/publication',                          'index');
+            Route::post('/publication',                        'store');
+            Route::get('/publication/{id}',                     'show');
+            Route::put('/publication/{id}',                     'update');
+            Route::delete('/publication/{id}',                  'destroy');
+        });
+
+        
+        Route::controller(PublicationManagementsController::class)->group(function () {
+            Route::get('/management',                          'index');
+            Route::post('/management',                         'store');
+            Route::get('/management/{id}',                     'show');
+            Route::put('/management/{id}',                     'update');
+            Route::delete('/management/{id}',                  'destroy');
+        });
+
     });
 
-      /* Editor Management */ //Manage by Editorial Staff
-    Route::controller(EditorialStaffsController::class)->group(function () {
-        Route::get('/editor',                          'index');
-        Route::get('/editor/{id}',                     'show');
-        Route::delete('/editor/{id}',                  'destroy');
-    });
 
-      /* Article Management */ //Manage by Author
-    Route::controller(ArticlesController::class)->group(function () {
-        Route::get('/article',                               'index');
-        Route::get('/article/{id}',                          'show');
-        Route::put('/article/title/{id}',                    'title')->name('article.title');
-        Route::put('/article/content/{id}',                  'content')->name('article.content');
-        Route::put('/article/file/{id}',                     'file')->name('article.file');
-        Route::post('/article',                              'store')->name('article.store');
-    });
+     /* Author only routes */
+    Route::middleware('author')->group(function () {
 
-    /* cover request Management */ //Manage by Author
-    Route::controller(CoverRequestsController::class)->group(function () {
-        Route::get('/cover',                               'index');
-        Route::get('/cover/{id}',                          'show');
-        Route::delete('/cover/{id}',                       'destroy');
-    });
+        /* Article Management */
+        Route::controller(ArticlesController::class)->group(function () {
+            Route::get('/article',                               'index');
+            Route::get('/article/{id}',                          'show');
+            Route::put('/article/title/{id}',                    'title')->name('article.title');
+            Route::put('/article/content/{id}',                  'content')->name('article.content');
+            Route::put('/article/file/{id}',                     'file')->name('article.file');
+            Route::post('/article',                              'store')->name('article.store');
+        });
+        
+        /* cover request Management */
+        Route::controller(CoverRequestsController::class)->group(function () {
+            Route::get('/cover',                               'index');
+            Route::get('/cover/{id}',                          'show');
+            Route::delete('/cover/{id}',                       'destroy');
+        });
 
-    //to author
-    Route::controller(AuthorLogsController::class)->group(function () {
-        Route::get('/authorLogs',                               'index');
-        Route::get('/authorLogs/{id}',                          'show');
-    });
 
-    //to editor
-    Route::controller(PublicationsController::class)->group(function () {
-        Route::get('/publication',                          'index');
-        Route::post('/publication',                        'store');
-        Route::get('/publication/{id}',                     'show');
-        Route::put('/publication/{id}',                     'update');
-        Route::delete('/publication/{id}',                  'destroy');
-    });
-
-    //to editor
-    Route::controller(PublicationManagementsController::class)->group(function () {
-        Route::get('/management',                          'index');
-        Route::post('/management',                         'store');
-        Route::get('/management/{id}',                     'show');
-        Route::put('/management/{id}',                     'update');
-        Route::delete('/management/{id}',                  'destroy');
+        /* Logs for Author */
+        Route::controller(AuthorLogsController::class)->group(function () {
+            Route::get('/authorLogs',                               'index');
+            Route::get('/authorLogs/{id}',                          'show');
+        });
     });
 
 });
